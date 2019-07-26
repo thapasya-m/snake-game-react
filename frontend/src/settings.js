b5756b10a676eff3be39a53fae5a04e7863b3d7a
8-bit-princess <thapasya.murali@gmail.com>
2019-07-27 00:07:41 +0530

    revert settings

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,2 +1,2 @@
-// export const BASE_API = "https://8-bit-princess-snake-game-react.glitch.me"
-export const BASE_API = "http://localhost:4200"
+export const BASE_API = "https://8-bit-princess-snake-game-react.glitch.me"
+// export const BASE_API = "http://localhost:4200"
